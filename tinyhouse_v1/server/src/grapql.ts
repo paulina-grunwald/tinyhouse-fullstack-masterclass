03c8cfc6664577138d8edd572704e4f2eabdae34
Paulina Grunwald <paulina.grunwald@gmail.com>
2020-11-19 07:14:00 +0100

    Add main query listin all the items and delete item mutation.

--- tinyhouse_v1/server/src/grapql.ts
+++ tinyhouse_v1/server/src/grapql.ts
@@ -1,11 +1,27 @@
-import { GraphQLSchema, GraphQLString, GraphQLObjectType } from "graphql";
+import { listings } from './listings';
+import { GraphQLSchema, GraphQLNonNull, GraphQLID, GraphQLInt, GraphQLFloat, GraphQLString, GraphQLObjectType, GraphQLList } from "graphql";
+
+const Listing = new GraphQLObjectType({
+  name: "Listing",
+  fields: {
+    id: { type: GraphQLNonNull(GraphQLID) },
+    title: { type: GraphQLNonNull(GraphQLString) },
+    image: { type: GraphQLNonNull(GraphQLString) },
+    address: { type: GraphQLNonNull(GraphQLString) },
+    price: { type: GraphQLNonNull(GraphQLInt) },
+    numOfGuests: { type: GraphQLNonNull(GraphQLInt) },
+    numOfBeds: { type: GraphQLNonNull(GraphQLInt) },
+    numOfBaths: { type: GraphQLNonNull(GraphQLInt) },
+    rating: { type: GraphQLNonNull(GraphQLFloat) }
+  }
+})
 
 const query = new GraphQLObjectType({
   name: "Query",
   fields: {
-    hello: {
-      type: GraphQLString,
-      resolve: () => "hello from query"
+    listings: {
+      type: GraphQLNonNull(GraphQLList(GraphQLNonNull(Listing))),
+      resolve: () => listings
     }
 
   }
@@ -14,11 +30,23 @@ const query = new GraphQLObjectType({
 const mutation = new GraphQLObjectType({
   name: "Mutation",
   fields: {
-    hello: {
-      type: GraphQLString,
-      resolve: () => "hello from mutation"
+    deleteListing: {
+      type: GraphQLNonNull(Listing),
+      args: {
+        id: {
+          type: GraphQLNonNull(GraphQLID)
+        }
+      },
+      resolve: (_root, { id }) => {
+        for (let i = 0; i < listings.length; i++) {
+          if (listings[i].id === id) {
+            return listings.splice(i, 1)[0]
+          }
+        }
+        throw new Error("failed to deleted listing")
+      }
     }
   }
-});
+})
 
 export const schema = new GraphQLSchema({query, mutation})
