c04de210c3f46c109eb2adabd5780e72badc655f
Paulina Grunwald <paulina.grunwald@gmail.com>
2020-11-19 07:34:52 +0100

    Define graphql schema including typedefs and resolvers.

--- tinyhouse_v1/server/src/index.ts
+++ tinyhouse_v1/server/src/index.ts
@@ -1,12 +1,11 @@
 import express from 'express';
 import { ApolloServer } from "apollo-server-express";
-import {schema} from './grapql'
+import {typeDefs, resolvers} from './graphql'
 
-import { listings } from "./listings";
 const app = express();
 const port = 9000;
 
-const server = new ApolloServer({ schema })
+const server = new ApolloServer({ typeDefs, resolvers })
 server.applyMiddleware({ app, path: "/api" });
 
 app.listen(port);
