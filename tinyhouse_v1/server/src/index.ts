ddcd9632984744c1a4f55eee7a0f5fafbbdf0614
Paulina Grunwald <paulina.grunwald@gmail.com>
2020-11-29 20:17:11 +0100

    Set up connection to mongodb. Set up env variables.

--- tinyhouse_v1/server/src/index.ts
+++ tinyhouse_v1/server/src/index.ts
@@ -1,13 +1,26 @@
-import express from 'express';
+require("dotenv").config();
+import express, { Application } from 'express';
 import { ApolloServer } from "apollo-server-express";
+import { connectDatabase } from './database/index';
 import {typeDefs, resolvers} from './graphql'
 
-const app = express();
-const port = 9000;
+const port = process.env.PORT;
 
-const server = new ApolloServer({ typeDefs, resolvers })
-server.applyMiddleware({ app, path: "/api" });
 
-app.listen(port);
+const mount = async (app: Application) => {
+  const db = await connectDatabase();
+  const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+    context: () => ({ db })
+  });
 
-console.log(`[app]: http://localhost:${port}`);
+  app.listen(port);
+  console.log(`[app]: http://localhost:${port}`);
+  const listings = await db.listings.find({}).toArray();
+  console.log(listings);
+}
+
+mount(express())
+
+
